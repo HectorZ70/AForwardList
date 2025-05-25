2b9db248ba74ac1c607f4e63292664a05034b3d0
HectorZ70 <hector.zornoza@enti.cat>
2025-05-25 11:35:50 +0200

    Project Initiated

---
+++ AForwardList/AForwardList/ForwardList.cpp
@@ -0,0 +1,82 @@
+#include "ForwardList.h"
+
+ForwardList::ForwardList()
+{
+
+}
+
+ForwardList::~ForwardList()
+{
+
+}
+
+void ForwardList::PushBack(int value)
+{
+
+}
+
+void ForwardList::PopBack()
+{
+
+}
+
+void ForwardList::PushFront(int value)
+{
+
+}
+
+void ForwardList::PopFront()
+{
+
+}
+
+bool ForwardList::IsEmpty() const
+{
+
+}
+
+int ForwardList::Front() const
+{
+
+}
+
+int ForwardList::Back() const
+{
+
+}
+
+int ForwardList::Size() const
+{
+
+}
+
+void ForwardList::Erase(int value)
+{
+
+}
+
+void ForwardList::Insert(int value, int position)
+{
+
+}
+
+void ForwardList::PushBackAverageNode()
+{
+
+}
+
+bool operator==(const ForwardList& l1, const ForwardList& l2)
+{
+
+}
+
+ForwardList operator+(const ForwardList& l1)
+{
+
+}
+
+std::ostream& operator<<(std::ostream& o, const ForwardList& l)
+{
+
+}
+
